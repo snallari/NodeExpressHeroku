e5989b58ef409595646003b54c5d00827ba98e58
nallari <shruti.nallari@capgemini.com>
2021-08-03 14:30:59 -0400

    server

--- server/server.js
+++ server/server.js
@@ -149,7 +149,7 @@ app.get('/getUser', function (req, response) {
     });
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8081, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
